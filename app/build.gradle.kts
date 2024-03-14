fb5baf96a51e254fb82061acd8ef440ff5d362c3
Ratnesh Navlakhe <ratnesh061288@gmail.com>
2024-03-14 18:25:19 +0530

    Implement document scanner ml kit, scan document show it on the app.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,9 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.android.gms.mlkit.document.scanner)
+    implementation(libs.coil.compose.version)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
